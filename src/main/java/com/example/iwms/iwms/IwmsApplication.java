0636f6cdf3254d118ef8b21b23d2b055ceaec4a9
Uzair Khan <jerry.khan7886@gmail.com>
2024-11-18 15:58:01 +0500

    init

---
+++ src/main/java/com/example/iwms/iwms/IwmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.iwms.iwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iwms/iwms/IwmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iwms.iwms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
